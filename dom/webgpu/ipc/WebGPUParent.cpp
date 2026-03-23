a675f00c72d91c4473d18670fc81819dd516d011
Jim Blandy <jimb@red-bean.com>
2026-03-23 04:38:29 +0000

    Bug 1870699: Use new `wgpu_server_buffer_map_blocking` for `GetFrontBufferSnapshot`. r=webgpu-reviewers,teoxoy
    Define a new function, `wgpu_server_buffer_map_blocking` that maps a
    wgpu buffer and blocks the calling thread until its contents are ready
    for access by the CPU.
    
    Use this function in `WebGPUParent::GetFrontBufferSnapshot`, rather
    than calling `wgpu_server_poll_all_devices` explicitly to wait for the
    buffer to be ready.
    
    Differential Revision: https://phabricator.services.mozilla.com/D285397

--- dom/webgpu/ipc/WebGPUParent.cpp
+++ dom/webgpu/ipc/WebGPUParent.cpp
@@ -1341,20 +1341,11 @@ ipc::IPCResult WebGPUParent::GetFrontBufferSnapshot(
   auto snapshotRequest = MakeUnique<ReadbackSnapshotRequest>(
       mContext.get(), data, bufferId, shmem, stride);
 
-  ffi::WGPUBufferMapClosure closure = {
-      &ReadbackSnapshotCallback,
-      reinterpret_cast<uint8_t*>(snapshotRequest.release())};
-
-  ErrorBuffer error;
-  ffi::wgpu_server_buffer_map(mContext.get(), data->mDeviceId, bufferId, 0,
-                              bufferSize, ffi::WGPUHostMap_Read, closure,
-                              error.ToFFI());
-  if (ForwardError(error)) {
-    return IPC_OK();
-  }
-
-  // Callback should be called during the poll.
-  ffi::wgpu_server_poll_all_devices(mContext.get(), true);
+  ffi::WGPUBufferMapAsyncStatus status = ffi::wgpu_server_buffer_map_blocking(
+      mContext.get(), data->mDeviceId, bufferId, 0, bufferSize,
+      ffi::WGPUHostMap_Read);
+  ReadbackSnapshotCallback(
+      reinterpret_cast<uint8_t*>(snapshotRequest.release()), status);
 
   // Check if ReadbackSnapshotCallback is called.
   MOZ_RELEASE_ASSERT(data->mReadbackSnapshotCallbackCalled == true);
