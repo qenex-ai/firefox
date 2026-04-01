3baa3220275a6b9bd8ea478451df65cc544adda0
kofoworola_shonuyi <skofoworola3@gmail.com>
2026-04-01 20:04:07 +0000

    Bug 1999012 - Fix sidebar-shown attribute in fullscreen and add guard clause for xpcshell r=jsudiaman
    Differential Revision: https://phabricator.services.mozilla.com/D291581

--- browser/components/sidebar/SidebarState.sys.mjs
+++ browser/components/sidebar/SidebarState.sys.mjs
@@ -77,6 +77,7 @@ export class SidebarState {
   };
   #launcherEverVisible = false;
   bookmarksExpandedFolders = [];
+  #fullscreen = false;
 
   /** @type {SidebarStateProps} */
   static defaultProperties = Object.freeze({
@@ -422,6 +423,19 @@ export class SidebarState {
       return true;
     }
     return DEFAULT_LAUNCHER_VISIBLE;
+  }
+
+  get fullscreen() {
+    return this.#fullscreen;
+  }
+
+  set fullscreen(val) {
+    if (this.#fullscreen === val) {
+      return;
+    }
+    this.#fullscreen = val;
+    // Re-run the update logic every time the fullscreen state changes.
+    this.#updateTabbrowser(this.launcherVisible);
   }
 
   get launcherVisible() {
@@ -783,9 +797,13 @@ export class SidebarState {
   }
 
   #updateTabbrowser(isSidebarShown) {
-    this.#controllerGlobal.document
-      .getElementById("tabbrowser-tabbox")
-      .toggleAttribute("sidebar-shown", isSidebarShown);
+    const doc = this.#controllerGlobal.document;
+    const tabbox = doc.getElementById("tabbrowser-tabbox");
+    if (!tabbox || !doc.documentElement) {
+      return;
+    }
+    const inFullscreen = doc.documentElement.hasAttribute("inDOMFullscreen");
+    tabbox.toggleAttribute("sidebar-shown", isSidebarShown && !inFullscreen);
   }
 
   get command() {
