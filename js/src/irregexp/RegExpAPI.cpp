411b18226df99453dc99395324fd205f7eac784c
Iain Ireland <iireland@mozilla.com>
2026-04-01 22:41:44 +0000

    Bug 2022634: Update zone shim r=spidermonkey-reviewers,dminor
    The main change here is moving the LifoAllocScope inside the Zone constructor instead of outside. This is necessary because irregexp starts defining its own zones in RegExpCodeGenerator.
    
    Differential Revision: https://phabricator.services.mozilla.com/D288791

--- js/src/irregexp/RegExpAPI.cpp
+++ js/src/irregexp/RegExpAPI.cpp
@@ -303,8 +303,7 @@ static bool CheckPatternSyntaxImpl(js::LifoAlloc& alloc,
                                    JS::RegExpFlags flags,
                                    RegExpCompileData* result,
                                    JS::AutoAssertNoGC& nogc) {
-  LifoAllocScope allocScope(&alloc);
-  Zone zone(allocScope.alloc());
+  Zone zone(&alloc);
 
   return RegExpParser::VerifyRegExpSyntax(&zone, stackLimit, input, inputLength,
                                           flags, result, nogc);
@@ -768,9 +767,8 @@ bool CompilePattern(JSContext* cx, MutableHandleRegExpShared re,
                     RegExpShared::CodeKind codeKind) {
   Rooted<JSAtom*> pattern(cx, re->getSource());
   JS::RegExpFlags flags = re->getFlags();
-  LifoAllocScope allocScope(&cx->tempLifoAlloc());
   HandleScope handleScope(cx->isolate);
-  Zone zone(allocScope.alloc());
+  Zone zone(&cx->tempLifoAlloc());
 
   RegExpCompileData data;
   {
